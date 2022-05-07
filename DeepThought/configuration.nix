102bb7c93d0782f31f7cdf7dd20803e7e2f1677b
Carlos Hernandez <carlos@hrndz.ca>
2022-05-07 17:09:30 -0600

    Setup a usable bash shell

--- DeepThought/configuration.nix
+++ DeepThought/configuration.nix
@@ -103,6 +103,20 @@ in
     ];
   };
 
+  programs.bash = {
+    shellAliases = {
+      l="exa -lh --group-directories-first -F --icons";
+      la = "exa -aalhF --group-directories-first --icons";
+      lt = "exa --tree --icons -d -a --ignore-glob '**/.git'";
+      tm = "tmux new-session -A -s main";
+      cat = "bat";
+      type="type -a";
+      mkdir="mkdir -p";
+    };
+    interactiveShellInit = ''
+      source "$(sk-share)/key-bindings.bash"
+    '';
+  };
 
   programs.neovim = {
     enable = true;
